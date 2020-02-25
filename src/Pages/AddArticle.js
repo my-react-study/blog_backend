b74f0d59a9650cd0f2c843addd7da5b02586acf3
tangmingheng <mhtang@thoughtworks.com>
2020-02-25 22:14:41 +0800

    加载文章类别

--- src/Pages/AddArticle.js
+++ src/Pages/AddArticle.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import marked from 'marked'
 import '../static/css/AddArticle.css'
 import { Row, Col, Input, Select, Button, DatePicker } from 'antd'
+import axios from 'axios'
+import servicePath from '../config/apiUrl'
 
 const { Option } = Select;
 const { TextArea } = Input
 
-function AddArticle() {
+function AddArticle(props) {
     const [articleId, setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
     const [articleTitle, setArticleTitle] = useState('')   //文章标题
     const [articleContent, setArticleContent] = useState('')  //markdown的编辑内容
@@ -30,6 +32,10 @@ function AddArticle() {
         smartypants: false,
     });
 
+    useEffect(() => {
+        getTypeInfo()
+    }, [])
+
     const changeContent = (e) => {
         setArticleContent(e.target.value)
         let html = marked(e.target.value)
@@ -40,6 +46,31 @@ function AddArticle() {
         setIntroducemd(e.target.value)
         let html = marked(e.target.value)
         setIntroducehtml(html)
+    }
+
+    //从中台得到文章类别信息
+    const getTypeInfo = () => {
+        axios({
+            method: 'get',
+            url: servicePath.getTypeInfo,
+            header: { 'Access-Control-Allow-Origin': '*' },
+            withCredentials: true
+        }).then(
+            res => {
+                if (res.data.data == "没有登录") {
+                    localStorage.removeItem('openId')
+                    props.history.push('/')
+                } else {
+                    setTypeInfo(res.data.data)
+                }
+
+            }
+        )
+    }
+
+    //选择类别后的便哈
+    const selectTypeHandler = (value) => {
+        setSelectType(value)
     }
 
     return (
@@ -54,8 +85,12 @@ function AddArticle() {
                         </Col>
                         <Col span={4}>
                             &nbsp;
-                            <Select defaultValue="Sign Up" size="large">
-                                <Option value="Sign Up">视频教程</Option>
+                            <Select defaultValue={selectedType} onChange={selectTypeHandler} size="large" >
+                                {
+                                    typeInfo.map((item, index) => {
+                                        return (<Option key={index} value={item.Id}>{item.typeName}</Option>)
+                                    })
+                                }
                             </Select>
                         </Col>
                     </Row>
